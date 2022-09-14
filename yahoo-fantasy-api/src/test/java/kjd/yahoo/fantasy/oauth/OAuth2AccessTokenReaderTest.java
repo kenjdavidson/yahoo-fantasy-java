425133447ed8e509849656e591a191c660e92c0c
kendavidson <ken.j.davidson@live.ca>
2022-09-14 15:01:53 +0000

    chore(test): disabled invalid tests

--- yahoo-fantasy-api/src/test/java/kjd/yahoo/fantasy/oauth/OAuth2AccessTokenReaderTest.java
+++ yahoo-fantasy-api/src/test/java/kjd/yahoo/fantasy/oauth/OAuth2AccessTokenReaderTest.java
@@ -5,12 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.github.scribejava.core.model.OAuth2AccessToken;
 
+@Disabled
 class OAuth2AccessTokenReaderTest {
 
 	static YahooOAuthTokenReader reader;
